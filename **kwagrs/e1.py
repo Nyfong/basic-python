2ef549b28f1aaa8acb67b15d6a070937c84add62
Nyfong <litongfong12@gmail.com>
2025-08-16 16:25:20 +0700

    using **kwgrs

---
+++ **kwagrs/e1.py
@@ -0,0 +1,9 @@
+# **kwargs → captures keyword arguments as a dict
+def my_fun(**kwats):
+    print(kwats)
+
+key="val"
+my_fun(key="val", key2="val2")  # it will print as a dictionary based on the function we return print
+
+#if  my_fun(key, key2="val2")  is error cuz the first argument is not a keyword argument
+# the first augument is a value so error will be occur
--- comprehensions/dict_comprehension/e1.py
+++ comprehensions/dict_comprehension/e1.py
@@ -1,9 +1,12 @@
 # {key_expr: value_expr for item in iterable if condition}
 
 nums = [1, 2, 3, 4]
-square_dicts = {x : x *2 for x in nums }
+
+square_dicts = {x : x **2 for x in nums }
+
+print(square_dicts) 
+
 
 # loop element in nums
 #and then take each element x and create a key value pair
 # where key is x and value is x * 2
-print(square_dicts) 
